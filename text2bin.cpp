6441aad53e399b71a2193530965ea7dd16fb63c5
Tatiana Borisova <tanyatik@yandex.ru>
2014-10-12 18:00:36 +0400

    Add text2bin source file

---
+++ text2bin.cpp
@@ -0,0 +1,27 @@
+// Converts text file with integer to binary format.
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main(int, char **argv) {
+    using std::ifstream;
+    using std::ofstream;
+    using std::ios_base;
+
+    ifstream fin(argv[1], ios_base::in);
+    ofstream fout(argv[2], ios_base::out | ios_base::binary);
+
+    int value = 0;
+    while (fin) {
+        fin >> value;
+        if (!fin) {
+            break;
+        }
+        std::cout << value << std::endl;
+        fout.write((const char *) &value, sizeof(value));
+    }
+    fin.close();
+    fout.close();
+
+    return 0;
+}
