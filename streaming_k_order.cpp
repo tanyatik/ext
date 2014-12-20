c90d812259ff99f48ad22d82eb3518582e2a27c7
Tatiana Borisova <tanyatik@yandex.ru>
2014-12-20 06:59:28 +0300

    Cleanup a bit

--- streaming_k_order.cpp
+++ streaming_k_order.cpp
@@ -19,7 +19,6 @@ using std::max;
 const size_t MAX_MEMORY = 2 * 1024 * 1024 * 1024;
 const size_t MAX_MEMORY_COUNT = MAX_MEMORY / sizeof(int);
 const size_t MAX_CHUNK = 75 * 1024 * 1024;
-const size_t MAX_HEIGHT = 30;
 
 
 struct Filter {
@@ -35,8 +34,7 @@ struct Filter {
 
 
 size_t GetChunkSize() {
-    return 4;
-    // return MAX_CHUNK;
+    return MAX_CHUNK;
 }
 
 
@@ -46,18 +44,18 @@ vector<int> MergeSubtrees(const vector<int>& left, const vector<int>& right, int
     vector<int> result;
 
     // While there are elements in the left or right runs
-    int num = 0;
+    size_t num = 0;
     while (num < left.size() + right.size()) {
         // If left run head exists and is <= existing right run head.
         if (begin_first < left.end() &&
                 (begin_second >= right.end() || !(*begin_second < *begin_first))) {
-            if ((num + 1) % thinning_coeff == 0) {
+            if ((num) % thinning_coeff == 0) {
                 result.push_back(*begin_first);  // Increment begin_first after using it as an index.
             }
             ++begin_first;
         }
         else {
-            if ((num + 1) % thinning_coeff == 0) {
+            if ((num) % thinning_coeff == 0) {
                 result.push_back(*begin_second);  // Increment begin_first after using it as an index.
             }
             begin_second++;  // Increment begin_second after using it as an index.
@@ -115,25 +113,22 @@ vector<int> BuildTree(Filter filter, size_t chunk_size, bool* stop, size_t* heig
 Filter GetNewFilter(Filter filter, size_t height, int k_order, const vector<int>& tree_root) {
     size_t level = height - 1;
     int pow2 = int(pow(2, level));
-    cerr << "pow2 " << pow2 << endl;
 
     int j1 = int(ceil(1.0 * k_order / pow2)) - int(level);
-    if (1 <= j1 && j1 <= tree_root.size()) {
-        cerr << "j1 " << j1 << endl;
+    if (1 <= j1 && j1 <= int(tree_root.size())) {
         int beta = tree_root.size() - j1;
         filter.right_range = tree_root.at(beta);
     }
 
-    int j2 = int(ceil(1.0 * k_order / pow2)) + 2;
-    if (1 <= j2 && j2 <= tree_root.size()) {
-        cerr << "j2 " << j2 << endl;
+    int j2 = int(ceil(1.0 * k_order / pow2));
+    if (1 <= j2 && j2 <= int(tree_root.size())) {
         int alpha = tree_root.size() - j2;
         filter.left_range = tree_root.at(alpha);
     }
 
 
     assert(filter.left_range == INT_MIN || filter.right_range == INT_MAX ||
-            filter.right_range - filter.left_range < pow2 * (pow2 - 1));
+            filter.right_range - filter.left_range <= pow2 * (pow2 - 1));
     return filter;
 }
 
@@ -151,24 +146,15 @@ Filter FirstPass(Filter filter, int k_order) {
 
 
 int SecondPass(Filter filter, int k_order) {
-    int count_before = 0;
-
-    // assert(filter.right_range - filter.left_range <= MAX_MEMORY_COUNT);
-
     bool stop = false;
     int count_right = 0;
-    cerr << "2nd pass " << endl;
     vector<int> data = ReadTreeChunk(filter, MAX_MEMORY_COUNT, &stop, &count_right);
-    cerr << "data size " << data.size() << endl;
-    cerr << "count right " << count_right << endl;
 
     assert(data.size() <= MAX_MEMORY_COUNT);
     assert(k_order >= count_right);
     k_order -= count_right;
-    assert(k_order <= data.size());
-    cerr << "k order " << k_order << endl;
+    assert(k_order <= int(data.size()));
 
-    cerr << "index in sorted " << std::distance(data.begin(), data.end() - k_order) << endl;
     std::nth_element(data.begin(), data.end() - k_order, data.end());
     return data[data.size() - k_order];
 }
@@ -177,26 +163,12 @@ int SecondPass(Filter filter, int k_order) {
 int KOrderStatistics(int k_order) {
     Filter filter = FirstPass(Filter(INT_MIN, INT_MAX), k_order);
 
-    cerr << "[" << filter.left_range << ", " << filter.right_range << "]" << endl;
     return SecondPass(filter, k_order);
 }
 
 
 void PrintStatistics(int k_order, int st) {
     cout << k_order << "-statictics of input stream is " << st << endl;
-}
-
-
-// TESTS
-void TestMergeSubtrees() {
-    cerr << "TEST" << endl;
-    vector<int> a = {11, 31, 41, 51, 71, 81};
-    vector<int> b = {2, 3, 4, 5, 8, 15, 24};
-    vector<int> res = MergeSubtrees(a, b, 2);
-    for (int r : res) {
-        cerr << r << " ";
-    }
-    cerr << endl;
 }
 
 
@@ -218,9 +190,6 @@ int main(int argc, char **argv) {
 
     int st = KOrderStatistics(k_order);
     PrintStatistics(k_order, st);
-
-    // TestMergeSubtrees();
-    // TestBuildTree();
 
     return 0;
 }
